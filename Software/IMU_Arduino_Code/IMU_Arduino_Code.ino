6ee815746b7a6ebc02ddc2545fffb0b7f880d8f7
Sean McFarlane <spmcfarlane@gmail.com>
2020-05-31 11:40:23 -0400

    Added SD card recording

--- Software/IMU_Arduino_Code/IMU_Arduino_Code.ino
+++ Software/IMU_Arduino_Code/IMU_Arduino_Code.ino
@@ -1,5 +1,6 @@
 ////////////////////////////////////////////////////////////
-// Arduino firmware for use with FreeSixCube processing example
+// FreeSixIMU with datalogger
+//
 ////////////////////////////////////////////////////////////
 
 
@@ -14,43 +15,69 @@
 #include "CommunicationUtils.h"
 #include "FreeSixIMU.h"
 #include <Wire.h>
-#include <SoftwareSerial.h>
-
-SoftwareSerial XBee(2,3);
+#include <SPI.h>
+#include <SD.h>
 
 
 float q[9]; //hold q values
-int rawq[8];
-float mag_readings[3];
-int magx;
-int magy;
-int magz;
-IST8310 magn = IST8310();
-
+int rawq[9];
+int chipSelect = 10; //Set chip select for SD card to 10
 // Set the FreeIMU object
 FreeSixIMU my3IMU = FreeSixIMU();
 
 void setup() {
  Serial.begin(115200);
+ Serial.print("Initializing SD card...");
+
+ // see if the card is present and can be initialized:
+ if (!SD.begin(chipSelect)) {
+  Serial.println("Card failed, or not present");
+  // don't do anything more:
+  while (1);
+ }
+ Serial.println("card initialized.");
  Wire.begin();
 
  delay(5);
  
  my3IMU.init();
 
+ // delete file from the SD card if it already exists
+ if (SD.exists("datalog.txt"))
+ {
+    SD.remove("datalog.txt");
+    Serial.println("Old data log file deleted");
+ }
+  
+
  Serial.println("Starting Data");
- 
+
 }
 
 
 void loop() { 
   my3IMU.getRawValues(rawq);
-  magn.getRaw(&magx,&magy,&magz);
-
- //serialPrintFloatArr(q, 4);
- Serial.println(magx);
- Serial.println(magy);
- Serial.println(magz);
- delay(100); //line break
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  String dataString = "";
+  
+  if (dataFile)
+  {
+    dataString = String(rawq[0]) + "," + String(rawq[1]) + "," + String(rawq[2]) + "," + 
+    String(rawq[3]) + ","  + String(rawq[4]) + "," + String(rawq[5]) + "," + 
+    String(rawq[6]) + "," + String(rawq[7]) + "," + String(rawq[8]);
+    dataFile.println(dataString);
+    dataFile.close();
+  }else
+  {
+    Serial.println(rawq[0]);
+    Serial.println(rawq[1]);
+    Serial.println(rawq[2]);
+    Serial.println(rawq[3]);
+    Serial.println(rawq[4]);
+    Serial.println(rawq[5]);
+    Serial.println(rawq[6]);
+    Serial.println(rawq[7]);
+    Serial.println(rawq[8]);
+  }
 
 }
